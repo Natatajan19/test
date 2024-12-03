1cb0b0480787c013919d98cb0c260080691bdb40
Natarajan <natarajan190706@gmail.com>
2024-12-03 22:25:33 +0530

    age

---
+++ age.c
@@ -0,0 +1,12 @@
+//
+// Created by natar on 03-12-2024.
+//
+#include <stdio.h>
+int main()
+{
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("You are %d years old", age);
+    return 0;
+}
